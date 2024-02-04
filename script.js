fecdfae19d23fab6eb4b115ab3c33381987ae1cb
Frank kamgang <143176710+Frankam487@users.noreply.github.com>
2024-02-04 09:34:53 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const navMenu = document.getElementById("nav-menu"),
   navToggle = document.getElementById("nav-toggle"),
   navClose = document.getElementById("nav-close");
 
+
 if (navToggle) {
   navToggle.addEventListener("click", () => {
     navMenu.classList.add("show-menu");
